2f7b70553f37c3662f279230b07083d08b1390f0
jayakrishna0511 <dontharaboinajayakrishna@gmail.com>
2025-05-22 14:09:56 +0530

    configured some dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Emoji-Tic-Tac-Toe/', // 👈 Required for GitHub Pages
   plugins: [react()],
-});
+})
